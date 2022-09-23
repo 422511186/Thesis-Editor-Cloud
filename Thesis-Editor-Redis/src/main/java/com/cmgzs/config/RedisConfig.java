1f489c7c28c2285869221c31daa194631623d9ef
huangzhenyu <422511186@qq.com>
2022-09-23 23:15:13 +0800

    Thesis-Editor-Redis - Redisson 分布式锁整合

--- Thesis-Editor-Redis/src/main/java/com/cmgzs/config/RedisConfig.java
+++ Thesis-Editor-Redis/src/main/java/com/cmgzs/config/RedisConfig.java
@@ -1,7 +1,17 @@
 package com.cmgzs.config;
 
+import com.cmgzs.utils.text.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.SingleServerConfig;
+import org.redisson.spring.cache.RedissonSpringCacheManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -10,20 +20,43 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * redis配置
- * 
+ *
  * @author hzy
  */
 @Configuration
 @EnableCaching
 @AutoConfigureBefore(RedisAutoConfiguration.class)
-public class RedisConfig extends CachingConfigurerSupport
-{
+@Slf4j
+public class RedisConfig extends CachingConfigurerSupport {
+
+
+    @Value("${redisson.host}")
+    private String host;
+
+    @Value("${redis.port}")
+    private String port;
+
+    @Value("${redis.password}")
+    private String password;
+
+    @Value("${redis.minimumIdleSize}")
+    private int minimumIdleSize;
+
+    @Value("${redis.connectionPoolSize}")
+    private int connectionPoolSize;
+
+    @Value("${redis.database}")
+    private int database;
+
+
     @Bean
-    @SuppressWarnings(value = { "unchecked", "rawtypes" })
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory)
-    {
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
@@ -39,5 +72,29 @@ public class RedisConfig extends CachingConfigurerSupport
 
         template.afterPropertiesSet();
         return template;
+    }
+
+    @Bean(destroyMethod = "shutdown", name = "redissonClient")
+    RedissonClient redisson() throws IOException {
+        Config config = new Config();
+        log.info(host);
+        log.info(port);
+        SingleServerConfig serverConfig = config.useSingleServer()
+                .setAddress("redis://" + host + ":" + port)
+                .setKeepAlive(true)
+                .setDatabase(database)
+                .setConnectionMinimumIdleSize(minimumIdleSize)
+                .setDnsMonitoringInterval(30000L)
+                .setConnectionPoolSize(connectionPoolSize);
+        if (StringUtils.isNotBlank(password)) {
+            serverConfig.setPassword(password);
+        }
+        return Redisson.create(config);
+    }
+
+    @Bean
+    CacheManager cacheManager(@Qualifier("redissonClient") RedissonClient redissonClient) {
+        Map config = new HashMap();
+        return new RedissonSpringCacheManager(redissonClient, config);
     }
 }
