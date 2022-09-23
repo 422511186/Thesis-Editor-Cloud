92ffa1140630324e87d6a690e79c67fd3a4e6ac4
huangzhenyu <422511186@qq.com>
2022-09-23 23:21:46 +0800

    Thesis-Editor-OpenFeign - 文件位置调整

--- Thesis-Editor-OpenFeign/src/main/java/com/cmgzs/interceptor/HeaderInterceptor.java
+++ Thesis-Editor-OpenFeign/src/main/java/com/cmgzs/interceptor/HeaderInterceptor.java
@@ -1,4 +1,4 @@
-package com.cmgzs.config;
+package com.cmgzs.interceptor;
 
 import com.cmgzs.utils.text.StringUtils;
 import feign.RequestInterceptor;
@@ -7,8 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+/**
+ * 请求时，带上请求头中的token
+ *
+ * @author huangzhenyu
+ * @date 2022/9/23
+ */
 @Slf4j
-public class HeaderIntercepter implements RequestInterceptor {
+public class HeaderInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
