b317a51dc9ab3e76e0651018eeeaee025cc544ce
huangzhenyu <422511186@qq.com>
2022-09-26 15:29:40 +0800

    1

--- Thesis-Editor-Archive/src/main/java/com/cmgzs/ArchiveApplication.java
+++ Thesis-Editor-Archive/src/main/java/com/cmgzs/ArchiveApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
+ *
  * @author huangzhenyu
  * @date 2022/9/10
  */
