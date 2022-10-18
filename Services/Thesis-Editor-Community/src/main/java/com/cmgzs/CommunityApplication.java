1d023c5d4f0ff26c12a7db578e4de0fc01e88c99
huangzhenyu <422511186@qq.com>
2022-10-18 12:25:41 +0800

    帖子 -- 接口测试完成

--- Services/Thesis-Editor-Community/src/main/java/com/cmgzs/CommunityApplication.java
+++ Services/Thesis-Editor-Community/src/main/java/com/cmgzs/CommunityApplication.java
@@ -2,11 +2,15 @@ package com.cmgzs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author huangzhenyu
  * @date 2022/10/16
  */
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CommunityApplication {
     public static void main(String[] args) {
